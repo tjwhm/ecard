9f88b1728bde5271d47cd2786bd063b8c8c7b1ad
Tzingtao Chow <Cyphexl@outlook.com>
2018-11-30 15:39:00 +0800

    ADD-chart-view-with-fake-data

--- front-end/vue-ecard/src/views/Detail.vue
+++ front-end/vue-ecard/src/views/Detail.vue
@@ -2,6 +2,7 @@
   <div class="detail">
     <Header subheadings="Detail"/>
     <div style="height: 20px;"/>
+    <div id="chart-container"></div>
     <div style="height: 70px;"/>
     <router-link to="/home"><button>返回 Home</button></router-link>
     <div style="height: 50px;"/>
@@ -19,9 +20,74 @@ export default {
   components: {
     Header,
     Footer
+  },
+  methods: {
+    initChart: function() {
+      let echarts = require("echarts");
+      console.log(document.getElementById("chart-container"));
+      var myChart = echarts.init(document.getElementById("chart-container"));
+      myChart.setOption({
+        tooltip: {},
+        yAxis: {},
+        xAxis: {
+          data: [
+            5,
+            20,
+            36,
+            10,
+            10,
+            20,
+            20,
+            36,
+            10,
+            10,
+            36,
+            20,
+            36,
+            10,
+            10,
+            10,
+            10,
+            20
+          ]
+        },
+        series: [
+          {
+            name: "Amount",
+            type: "bar",
+            data: [
+              5,
+              20,
+              36,
+              10,
+              10,
+              20,
+              20,
+              36,
+              10,
+              10,
+              36,
+              20,
+              36,
+              10,
+              10,
+              10,
+              10,
+              20
+            ]
+          }
+        ]
+      });
+    }
+  },
+  mounted() {
+    this.initChart();
   }
 };
 </script>
 
 <style scoped lang="scss">
+#chart-container {
+  height: 300px;
+}
 </style>
