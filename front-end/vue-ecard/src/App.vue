8d793d7edcbfa0b10affa29d82303720b8fbb467
tjwhm <759361109@qq.com>
2018-12-16 22:15:16 +0800

    完成管理员界面的部分前后端对接请求

--- front-end/vue-ecard/src/App.vue
+++ front-end/vue-ecard/src/App.vue
@@ -9,168 +9,169 @@
 </template>
 
 <script>
-import Vue from "vue";
-import * as ModalDialogs from "vue-modal-dialogs";
-Vue.use(ModalDialogs);
+    import Vue from "vue";
+    import * as ModalDialogs from "vue-modal-dialogs";
+    Vue.use(ModalDialogs);
 
-export default {
-  name: "app"
-};
+    export default {
+        name: "app"
+    };
 </script>
 
 <style lang="scss">
-@font-face {
-  font-family: "Directors";
-  font-weight: bold;
-  src: url("assets/fonts/tt_directors_bold-webfont.woff2") format("woff2"),
+
+  @import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+  @font-face {
+    font-family: "Directors";
+    font-weight: bold;
+    src: url("assets/fonts/tt_directors_bold-webfont.woff2") format("woff2"),
     url("assets/fonts/tt_directors_bold-webfont.woff") format("woff");
-  font-style: normal;
-}
-
-@font-face {
-  font-family: "Directors";
-  font-weight: normal;
-  src: url("assets/fonts/tt_directors_regular-webfont.woff2") format("woff2"),
-    url("assets/fonts/tt_directors_regular-webfont.woff") format("woff");
-  font-style: normal;
-}
-
-@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
-
-* {
-  font-family: "Montserrat", "Avenir", "Segoe UI", Helvetica, Arial, "等线",
-    "微软雅黑", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-weight: 300;
-  transition: all 0.2s;
-  box-sizing: border-box;
-}
-
-*::selection {
-  background-color: #2f5b6b;
-  color: #eee;
-}
-
-a {
-  color: inherit;
-}
-
-a:hover,
-.clickable:hover {
-  opacity: 0.8;
-  text-decoration: none;
-}
-
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease;
-}
-
-@mixin sized-input() {
-  padding: 8px 15px;
-  font-weight: bold;
-  font-size: 20px;
-}
-
-button {
-  @include sized-input();
-  outline: none;
-  box-shadow: none;
-  background: none;
-  border: none;
-  background-color: #eee;
-  color: #141414;
-  cursor: pointer;
-  margin-right: 25px;
-  min-width: 120px;
-  display: inline-block;
-}
-
-button:hover {
-  transform: translateY(3px);
-  opacity: 0.9;
-}
-
-button:active {
-  transform: translateY(3px) scale(1.03);
-}
-
-input {
-  @include sized-input();
-  font-size: 18px;
-  border: none;
-  border-bottom: 1px solid currentColor;
-  background: none;
-  color: #eee;
-}
-
-input:focus {
-  outline: none;
-}
-
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  /* display: none; <- Crashes Chrome on hover */
-  -webkit-appearance: none;
-  margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
-}
-
-input {
-  margin-right: 30px;
-}
-
-.font-condensed {
-  font-family: "Directors", "FF Din", "Franklin Gothic", "等线", "微软雅黑",
-    sans-serif;
-}
-
-.left {
-  float: left;
-}
-
-.right {
-  float: right;
-}
-
-.clearfix {
-  float: auto;
-}
-
-body {
-  background: #141414;
-  margin-top: 20vh;
-  margin-bottom: 20vh;
-  color: #eee;
-}
-
-.fixed-width-container {
-  max-width: 900px;
-  margin: 0 auto;
-}
-
-.flex-cashblock-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-@media screen and (max-width: 980px) {
-  .fixed-width-container {
-    width: 87%;
+    font-style: normal;
   }
 
-  img {
-    max-width: 100%;
+  @font-face {
+    font-family: "Directors";
+    font-weight: normal;
+    src: url("assets/fonts/tt_directors_regular-webfont.woff2") format("woff2"),
+    url("assets/fonts/tt_directors_regular-webfont.woff") format("woff");
+    font-style: normal;
+  }
+
+
+  * {
+    font-family: "Montserrat", "Avenir", "Segoe UI", Helvetica, Arial, "等线",
+    "微软雅黑", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-weight: 300;
+    transition: all 0.2s;
+    box-sizing: border-box;
+  }
+
+  *::selection {
+    background-color: #2f5b6b;
+    color: #eee;
+  }
+
+  a {
+    color: inherit;
+  }
+
+  a:hover,
+  .clickable:hover {
+    opacity: 0.8;
+    text-decoration: none;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.2s ease;
+  }
+
+  @mixin sized-input() {
+    padding: 8px 15px;
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+  button {
+  @include sized-input();
+    outline: none;
+    box-shadow: none;
+    background: none;
+    border: none;
+    background-color: #eee;
+    color: #141414;
+    cursor: pointer;
+    margin-right: 25px;
+    min-width: 120px;
+    display: inline-block;
+  }
+
+  button:hover {
+    transform: translateY(3px);
+    opacity: 0.9;
+  }
+
+  button:active {
+    transform: translateY(3px) scale(1.03);
+  }
+
+  input {
+  @include sized-input();
+    font-size: 18px;
+    border: none;
+    border-bottom: 1px solid currentColor;
+    background: none;
+    color: #eee;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    /* display: none; <- Crashes Chrome on hover */
+    -webkit-appearance: none;
+    margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+  }
+
+  input {
+    margin-right: 30px;
+  }
+
+  .font-condensed {
+    font-family: "Directors", "FF Din", "Franklin Gothic", "等线", "微软雅黑",
+    sans-serif;
+  }
+
+  .left {
+    float: left;
+  }
+
+  .right {
+    float: right;
+  }
+
+  .clearfix {
+    float: auto;
+  }
+
+  body {
+    background: #141414;
+    margin-top: 20vh;
+    margin-bottom: 20vh;
+    color: #eee;
+  }
+
+  .fixed-width-container {
+    max-width: 900px;
+    margin: 0 auto;
   }
 
   .flex-cashblock-container {
-    flex-wrap: wrap;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  @media screen and (max-width: 980px) {
+    .fixed-width-container {
+      width: 87%;
+    }
+
+    img {
+      max-width: 100%;
+    }
+
+    .flex-cashblock-container {
+      flex-wrap: wrap;
     .cashblock {
       margin-right: 20px;
     }
@@ -180,5 +181,5 @@ body {
   button {
     margin-bottom: 20px;
   }
-}
+  }
 </style>
--- front-end/vue-ecard/src/views/Admin.vue
+++ front-end/vue-ecard/src/views/Admin.vue
@@ -4,30 +4,30 @@
     <div style="height: 70px;"/>
     <TitleCluster title-zh="为消费者充值" title-en="Record deposit"></TitleCluster>
     <div>
-      <input type="text" placeholder="消费者帐号">
-      <input type="number" placeholder="充值额">
-      <button @click="comfirmTopup">确认充值</button>
+      <input v-model.number="topupInfo.card_id" type="number" placeholder="消费者卡号">
+      <input v-model.number="topupInfo.value" type="number" placeholder="充值额">
+      <button @click="confirmTopup">确认充值</button>
     </div>
     <div style="height: 50px;"/>
     <TitleCluster title-zh="为商家提现" title-en="Record withdraw"></TitleCluster>
     <div>
-      <input type="text" placeholder="商家帐号">
-      <input type="number" placeholder="取现额">
-      <button>确认提现</button>
+      <input v-model.number="withdrawInfo.card_id" type="number" placeholder="商家卡号">
+      <input v-model.number="withdrawInfo.value" type="number" placeholder="取现额">
+      <button @click="confirmWithdraw">确认提现</button>
     </div>
     <div style="height: 50px;"/>
     <TitleCluster title-zh="查询用户流水" title-en="Spending Details Lookup"></TitleCluster>
     <div>
-      <input type="text" placeholder="用户帐号">
-      <button v-on:click="fetchLookupRequest">查询近期活动</button>
+      <input v-model.number="recordLookupInfo.card_id" type="number" placeholder="用户帐号">
+      <button @:click="fetchLookupRequest">查询近期活动</button>
     </div>
     <div style="height: 50px;"/>
     <DataTable :records="this.records"></DataTable>
     <div style="height: 50px;"/>
     <TitleCluster title-zh="补办记录" title-en="Record replacement"></TitleCluster>
     <div>
-      <input type="text" placeholder="消费者帐号">
-      <button>确认补办完成（解除挂失状态）</button>
+      <input v-model.number="reactivateInfo.card_id" type="number" placeholder="消费者卡号">
+      <button @click="confirmReactivation">确认补办完成（解除挂失状态）</button>
     </div>
     <div style="height: 50px;"/>
     <Footer />
@@ -52,29 +52,54 @@ export default {
   data() {
     return {
       records: undefined,
-      metadata: undefined
+      metadata: undefined,
+      topupInfo: {},
+      withdrawInfo: {},
+      recordLookupInfo: {},
+      reactivateInfo: {}
     };
   },
   methods: {
-    fetchLookupRequest: function() {
-      let sourceUrl = "https://api.myjson.com/bins/193so2";
-      fetch(sourceUrl)
-        .then(response => response.json())
-        .then(json => {
-          this.records = json.data;
-        });
-    },
-    async comfirmTopup() {
+    async confirmTopup() {
       if (await messageBox("Confirm", "是否确认充值？")) {
         this.$http
-          .post("balance", {})
+          .post("balance", topupInfo)
           .then(response => response.json())
           .then(json => {
             console.log(json);
             alertBox("Succeed", "充值成功", "关闭");
           });
       }
-    }
+    },
+    async confirmWithdraw() {
+      if (await messageBox("Confirm", "是否确认为商家提现？")) {
+        this.$http
+          .post("balance", withdrawInfo)
+          .then(response => response.json())
+          .then(json => {
+            console.log(json);
+            alertBox("Succeed", "提现成功", "关闭");
+          });
+      }
+    },
+    fetchLookupRequest() {
+      this.$http.get('records', recordLookupInfo)
+        .then(response => response.json())
+        .then(json => {
+          this.records = json.data;
+        });
+    },
+    async confirmReactivation() {
+      if (await messageBox("Confirm", "是否确认补办完成并解挂？")) {
+        this.$http
+          .put("card_status", reactivateInfo)
+          .then(response => response.json())
+          .then(json => {
+            console.log(json);
+            alertBox("Succeed", "解挂成功", "关闭");
+          });
+      }
+    },
   },
   mounted() {}
 };
