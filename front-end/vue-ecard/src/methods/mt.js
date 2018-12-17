fec998f2748145c2ec5ccfe531e50493c9da4291
tjwhm <759361109@qq.com>
2018-12-17 20:28:38 +0800

    删除冗余mt

--- front-end/vue-ecard/src/methods/mt.js
+++ front-end/vue-ecard/src/methods/mt.js
@@ -1,13 +1,23 @@
 module.exports = {
-  toMetadata: function(originalData) {
-    let res = {};
-    res["currentBalance"] = originalData[0].latest_balance.toFixed(2);
-    res["lastAmount"] = originalData[0].value.toFixed(2);
-    let sumAmount = originalData
-      .map(record => record.value)
-      .reduce((current, accu) => current + accu, 0);
-    res["recentTotal"] = sumAmount.toFixed(2);
-    res["dailyAverage"] = (sumAmount / originalData.length).toFixed(2);
+  toMetadata: function (originalData) {
+    let res = {
+      currentBalance: 0,
+      lastAmount: 0,
+      recentTotal: 0,
+      dailyAverage: 0
+    };
+    if (originalData.length) {
+      res["currentBalance"] = originalData[0].latest_balance;
+      res["lastAmount"] = originalData[0].value;
+      let sumAmount = originalData
+        .map(record => record.value)
+        .reduce((current, accu) => current + accu, 0);
+      res["recentTotal"] = sumAmount;
+      res["dailyAverage"] = (sumAmount / originalData.length);
+    }
+    for (let pr in res) {
+      res[pr] = res[pr].toFixed(2)
+    }
     return res;
   }
 };
