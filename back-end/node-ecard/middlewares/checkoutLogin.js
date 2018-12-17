8e217522e19035da5e7096749932d41f218baa47
zhijie lee <zhijielee1998@163.com>
2018-12-17 19:39:18 +0800

    debug

--- back-end/node-ecard/middlewares/checkoutLogin.js
+++ back-end/node-ecard/middlewares/checkoutLogin.js
@@ -6,10 +6,8 @@
  */
 module.exports = (req,res,next)=>{
     console.log(req.session);
-    req.session = {
-        "user_number":3016202253,
-        "location":"爱湘菜"
-    };
+    req.session.user_number = 3016202253;
+    req.session.location = "爱湘菜";
     if(!req.session.user_number){
         res.send(JSON.stringify({
             "error_code": 1000,
